4f23e949ad63aeb51b45bb013617b67569dac4cb
Fahmidur-arnob <arnobdev93@gmail.com>
2022-11-24 20:15:12 +0600

    Created another section for the home section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import HomeCard from '../HomeCard/HomeCard';
+import HomeQuotes from '../HomeQuotes/HomeQuotes';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <HomeCard></HomeCard>
+            <HomeQuotes></HomeQuotes>
         </div>
     );
 };
