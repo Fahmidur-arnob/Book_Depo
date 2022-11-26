e7cab3e64f634ade15e8d4b199d494599ee2b0ee
Fahmidur-arnob <arnobdev93@gmail.com>
2022-11-26 19:49:17 +0600

    Done with env things

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
-import app from '../../Firebase/Firebase.config';
+import app from "../../Firebase/Firebase.config";
 
 
 const auth = getAuth(app);
